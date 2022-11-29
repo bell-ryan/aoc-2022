0707dc58cfe11d32f8b1e77cd7e504996e63300a
Ryan <bellrd07@gmail.com>
2022-11-29 11:36:57 -0500

    move get input to utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,42 @@
 package utils
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
 func ErrorCheck(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+/*
+Get input data by day.
+@returns a multi line string as given by AoC
+*/
+func GetInputData(day int) string {
+	client := http.Client{}
+	url := fmt.Sprintf("https://adventofcode.com/2022/day/%d/input", day)
+	session := os.Getenv("AOC_SESSION")
+	req, err := http.NewRequest("GET", url, nil)
+
+	// The errors should really be returned and handled else where. I am just doing this to speed things along
+	ErrorCheck(err)
+
+	req.Header = http.Header{
+		"Content-Type": {"application/json"},
+		"Cookie":       {fmt.Sprintf("session=%s", session)},
+	}
+
+	res, err := client.Do(req)
+	ErrorCheck(err)
+
+	body, err := io.ReadAll(res.Body)
+	ErrorCheck(err)
+
+	return strings.TrimSuffix(string(body), "\n")
 }
